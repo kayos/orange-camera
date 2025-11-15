f6109eb2c679903e40316b979e501582faa18985
kayos <4426302+kayos@users.noreply.github.com>
2025-11-15 16:46:05 +0000

    Wrap around video

--- record.py
+++ record.py
@@ -4,10 +4,18 @@ import time
 import os
 
 video_directory = "videos"
+encoder = H264Encoder()
+file_index = 0
 
+def get_file_extension(encoder):
+    if (isinstance(encoder, H264Encoder)):
+        return "h264"
+    elif(isinstance(encoder, MJPEGEncoder)):
+        return "mjpeg"
+    
 
-def get_filename(postfix: int):
-    return os.path.abspath(f'{os.getcwd()}\\{video_directory}\\{postfix:04}.h264')
+def get_filename(index: int):
+    return os.path.abspath(f'{os.getcwd()}/{video_directory}/{index:04}.{get_file_extension(encoder)}')
 
 if not os.path.exists(video_directory):
     os.mkdir(video_directory)
@@ -21,14 +29,14 @@ except IndexError:
 video_config = picam2.create_video_configuration()
 picam2.configure(video_config)
 
-file_index = 0
-
 while True:
-    encoder = H264Encoder()
     filename = get_filename(file_index)
+    print ("Start " + filename)
     picam2.start_recording(encoder, filename, quality=Quality.MEDIUM)
     time.sleep(60)
     picam2.stop_recording()
-    file_index += 1
+    print ("Stop")
+    file_index +=1
     if file_index > 11:
         file_index = 0
+    
